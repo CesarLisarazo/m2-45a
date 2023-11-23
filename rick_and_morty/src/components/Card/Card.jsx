99e37c0a0e7f151189d73c5e6d9c6522b1e5af14
CesarLisarazo <cesarlisarazo@gmail.com>
2023-11-23 17:04:46 -0300

    style

--- rick_and_morty/src/components/Card/Card.jsx
+++ rick_and_morty/src/components/Card/Card.jsx
@@ -5,15 +5,14 @@ let Card= ({id,name,status,species, gender,origin,image,onClose,})=> {
 
   return (
     <div className="card">
+      <p className='charId'>{id}</p>
+      <div className=' charidbackground'></div>
       <button  className="closeCard" onClick={()=>onClose(id)}>Close card</button>
       <h2 className='charName'><p>{name}</p></h2>
     <Link to ={`/Detail/${id}`}>
       <img src={image} alt={name} />
       </Link>
-      <h2>{status}</h2>
-      <h2>{species}</h2>
-      <h2>{gender}</h2>
-      <h2>{origin}</h2>
+
     </div>
   );
 }
