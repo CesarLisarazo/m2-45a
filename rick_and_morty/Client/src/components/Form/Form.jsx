83fd17c358864b02a0f5d27f3c72bccad5711d9c
CesarLisarazo <cesarlisarazo@gmail.com>
2023-12-15 03:32:57 -0300

    style

--- rick_and_morty/Client/src/components/Form/Form.jsx
+++ rick_and_morty/Client/src/components/Form/Form.jsx
@@ -27,40 +27,47 @@ event.preventDefault();
 <div className='banner'>
   <img src='https://upload.wikimedia.org/wikipedia/commons/b/b1/Rick_and_Morty.svg'></img>
 </div>
-     
+     <div className="contains">
       <form onSubmit={handleSubmit}className="formCard">
       <div className =" formEmail">
-        <label  htmlFor="email"><p className="email">Email:</p></label>
+        <label  htmlFor="email"><h2 className="email">Email:</h2></label>
         <input
+        placeholder="Cesar@henry.com"
+        className="formInput"
         autoComplete="off"
           onChange={handleChange}
           value={userData.email}
           type="text"
           name="email"
-        /><div className="emailErrors">
+        />
+        
+        <div className="emailErrors">
     
-        {errors.e2 ? <p style={{color:"coral"}}>The email must contain an @</p> : <p style={{color:"rgb(179, 240, 213)"}}>The email must contain an @ ✔</p>}
-        {errors.e3 ? <p style={{color:"coral"}}>It cannot have more than 25 characters</p> : <p style={{color:"rgb(179, 240, 213)"}}>It cannot have more than 25 characters ✔</p>}
+        {errors.e2 ? <p style={{color:"red"}}>The email must contain an @ ✖</p> : <p style={{color:" rgb(17, 80, 119)"}}>The email must contain an @ ✔</p>}
+        {errors.e3 ? <p style={{color:"red"}}>It cannot have more than 25 characters ✖</p> : <p style={{color:" rgb(17, 80, 119)"}}>It cannot have more than 25 characters ✔</p>}
         </div>
         </div>
-      
+      <br></br>
         <div className =" formPass">
-        <label  htmlFor="password"><p className="Password" >Password:</p></label>
+        <label  htmlFor="password"><h2 className="Password" >Password:</h2></label>
         <input
+        placeholder="casa1234"
+        className="formInput"
            autoComplete="off"
           onChange={handleChange}
           value={userData.password}
           type="password"
           name="password"
         /><div className="passErrors">
-         {errors.p1 ? <p style={{color:"coral"}}>It must have at least one number </p> : <p style={{color:"rgb(179, 240, 213)"}}>It must have at least one number ✔</p>}
-         {errors.p2 ? <p style={{color:"coral"}}>It must have between 6 and 10 characters </p> : <p style={{color:"rgb(179, 240, 213)"}}>It must have between 6 and 10 characters ✔</p>}
+         {errors.p1 ? <p style={{color:"red"}}>It must have at least one number ✖</p> : <p style={{color:"rgb(17, 80, 119)"}}>It must have at least one number ✔</p>}
+         {errors.p2 ? <p style={{color:"red"}}>It must have between 6 and 10 characters ✖</p> : <p style={{color:"rgb(17, 80, 119)"}}>It must have between 6 and 10 characters ✔</p>}
          </div>
        
         </div>
      
-        <button disabled ={errors.e1|| errors.e2||  errors.e3|| errors.p1 ||errors.p2} className='showButton'  type="submit"> Submit</button>
+        <button disabled ={errors.e1|| errors.e2||  errors.e3|| errors.p1 ||errors.p2} className='subButton'  type="submit"> Submit</button>
       </form>
+      </div>
     </div>
   );
 };
