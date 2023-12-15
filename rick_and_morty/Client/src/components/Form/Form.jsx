10606a8a36ed63a3e569964e7adaeda230d28484
CesarLisarazo <cesarlisarazo@gmail.com>
2023-12-15 01:17:22 -0300

    style

--- rick_and_morty/Client/src/components/Form/Form.jsx
+++ rick_and_morty/Client/src/components/Form/Form.jsx
@@ -28,38 +28,38 @@ event.preventDefault();
   <img src='https://upload.wikimedia.org/wikipedia/commons/b/b1/Rick_and_Morty.svg'></img>
 </div>
      
-      <form onSubmit={handleSubmit}className="formCard gradient-border ">
+      <form onSubmit={handleSubmit}className="formCard">
       <div className =" formEmail">
-        <label  htmlFor="email">Email:</label>
+        <label  htmlFor="email"><p className="email">Email:</p></label>
         <input
         autoComplete="off"
           onChange={handleChange}
           value={userData.email}
           type="text"
           name="email"
-        />
-        {errors.e1 ? <p style={{color:"coral"}}>Enter email</p> : <p style={{color:"rgb(179, 240, 213)"}}>Enter email</p>}
-        {errors.e2 ? <p style={{color:"coral"}}>The email must contain an @</p> : <p style={{color:"rgb(179, 240, 213)"}}>The email must contain an @</p>}
-        {errors.e3 ? <p style={{color:"coral"}}>It cannot have more than 25 characters</p> : <p style={{color:"rgb(179, 240, 213)"}}>It cannot have more than 25 characters</p>}
-      
+        /><div className="emailErrors">
+    
+        {errors.e2 ? <p style={{color:"coral"}}>The email must contain an @</p> : <p style={{color:"rgb(179, 240, 213)"}}>The email must contain an @ ✔</p>}
+        {errors.e3 ? <p style={{color:"coral"}}>It cannot have more than 25 characters</p> : <p style={{color:"rgb(179, 240, 213)"}}>It cannot have more than 25 characters ✔</p>}
+        </div>
         </div>
       
         <div className =" formPass">
-        <label htmlFor="password">Password:</label>
+        <label  htmlFor="password"><p className="Password" >Password:</p></label>
         <input
            autoComplete="off"
           onChange={handleChange}
           value={userData.password}
           type="password"
           name="password"
-        />
-         {errors.p1 ? <p style={{color:"coral"}}>It must have at least one number</p> : <p style={{color:"rgb(179, 240, 213)"}}>It must have at least one number</p>}
-         {errors.p2 ? <p style={{color:"coral"}}>It must have between 6 and 10 characters</p> : <p style={{color:"rgb(179, 240, 213)"}}>It must have between 6 and 10 characters</p>}
-     
+        /><div className="passErrors">
+         {errors.p1 ? <p style={{color:"coral"}}>It must have at least one number </p> : <p style={{color:"rgb(179, 240, 213)"}}>It must have at least one number ✔</p>}
+         {errors.p2 ? <p style={{color:"coral"}}>It must have between 6 and 10 characters </p> : <p style={{color:"rgb(179, 240, 213)"}}>It must have between 6 and 10 characters ✔</p>}
+         </div>
        
         </div>
      
-        <button disabled ={errors.e1|| errors.e2||  errors.e3|| errors.p1 ||errors.p2} className='cardButton'  type="submit"> Submit</button>
+        <button disabled ={errors.e1|| errors.e2||  errors.e3|| errors.p1 ||errors.p2} className='showButton'  type="submit"> Submit</button>
       </form>
     </div>
   );
